e0a816f496923c010f4611f14c1c931e78888d3f
PedroCarnelossi <kakinelossi@gmail.com>
2026-03-23 17:04:58 -0300

    model

---
+++ GerenciamentodePedidos/src/main/java/com/pm/GerenciamentodePedidos/GerenciamentodePedidosApplication.java
@@ -0,0 +1,13 @@
+package com.pm.GerenciamentodePedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentodePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentodePedidosApplication.class, args);
+	}
+
+}
---
+++ GerenciamentodePedidos/src/main/java/com/pm/GerenciamentodePedidos/models/PedidosModel.java
@@ -0,0 +1,52 @@
+package com.pm.GerenciamentodePedidos.models;
+
+import jakarta.persistence.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_pedido")
+public class PedidosModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "dataPedido")
+    private LocalDateTime data;
+    @Column(name = "valorTotalPedido")
+    private BigDecimal valorTotal;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
